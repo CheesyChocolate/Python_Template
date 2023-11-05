c37978c00aee3882e8e5a4f39ba8c7e63428623d
CheesyChocolate <dev@behnamlal.xyz>
2023-11-05 15:40:22 +0000

    populated the template with a basic project structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='Python_Template',
+    version='0.1.0',
+    description='a project template for python',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Suren Lockwood',
+    author_email='dev@behnamlal.xyz',
+    url='https://github.com/CheesyChocolate/Python_Template',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
---
+++ src/core.py
@@ -0,0 +1,11 @@
+from . import helpers
+
+def get_hmm():
+    """Get a thought."""
+    return 'hmmm...'
+
+
+def hmm():
+    """Contemplation..."""
+    if helpers.get_answer():
+        print(get_hmm())
